b0c38765bee6483afee0febf2e7efa16198ceb83
yschoi <yschoi9733@gmail.com>
2025-01-24 11:56:31 +0900

    feat: DB 정보 숨김 처리

--- mariadb.js
+++ mariadb.js
@@ -2,10 +2,10 @@ const mariadb = require('mysql2');
 
 // DB와 연결 통로 생성
 const connection = mariadb.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'bookShop',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PW,
+  database: process.env.MYSQL_DB,
   dateStrings: true,
 });
 
